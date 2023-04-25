4fc7a1c61b833abea86fa69bc3365ed1a5381be1
Voyager_lqg7 <stepanenko.vadik@bk.ru>
2023-04-25 19:30:17 +0200

    Updated Api Models

--- Backend.Api/ApiModels/UserModel.cs
+++ Backend.Api/ApiModels/UserModel.cs
@@ -6,6 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public ApplicationFileModel Avatar { get; set; }
         public int TotalFollowers { get; set; }
         public int TotalFollowsTo { get; set; }
     }
